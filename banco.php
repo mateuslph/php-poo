94348c06c08bc2cf1dce5280562f5aa4fa174a96
mateuslph <mlptche@gmail.com>
2022-01-13 21:51:43 -0300

    arquivos de poo em php

---
+++ banco.php
@@ -0,0 +1,30 @@
+<?php
+
+require_once 'src/Conta.php';
+require_once 'src/Titular.php';
+require_once 'src/CPF.php';
+
+$vinicius = new Titular(new CPF('123.456.789-10'), 'Vinicius Dias');  /* esta criando primeiramente o cpf do titular, em seguida o 
+titular "Vinicius Dias" */
+$primeiraConta = new Conta($vinicius);  /* esta criando uma nova conta com o nome "$primeiraConta" com o titular "$vinicius", que 
+anteriormente foi preenchido */
+$primeiraConta->deposita(500);
+$primeiraConta->saca(300);
+
+echo $primeiraConta->recuperaNomeTitular() . PHP_EOL;
+echo $primeiraConta->recuperaCpfTitular() . PHP_EOL;
+echo $primeiraConta->recuperaSaldo() . PHP_EOL;
+
+$patricia = new Titular(new CPF('698.549.548-10'), 'Patricia');  // cria um novo titular com o nome de "$patricia"
+$segundaConta = new Conta($patricia);  // cria uma nova conta com o nome de "$segundaConta"
+var_dump($segundaConta);  // essa funcao mostra o conteudo de uma variavel de forma estruturada
+
+$outra = new Conta(new Titular(new CPF('123.654.789-01'), 'Abcdefg'));
+unset($segundaConta);  /* exclui a referencia na memoria da conta "$segundaConta", e com isso o "Garbage Collector" detecta um 
+espaco na memoria ocupado e nao util, e o exclui tambem */
+echo Conta::recuperaNumeroDeContas();  /* aqui esta acessando o metodo que obtem o numero de vezes que a classe foi instanciada,
+decrementando as vezes em que esse objeto foi abandonado e o "Garbage Collector" procedeu */
+
+echo $primeiraConta->transfere(50,$outra) . PHP_EOL;
+var_dump($outra);
+echo $primeiraConta->recuperaSaldo() . PHP_EOL;
---
+++ src/Conta.php
@@ -0,0 +1,81 @@
+<?php
+
+class Conta
+{
+    private $titular;  // atributo da "Conta"
+    private $saldo;  // atributo da "Conta"
+    private static $numeroDeContas = 0;  /* esse atributo eh "static" porque fica fixado na classe "Conta", e nao eh 
+    requisitado ao criar o objeto. Porque cada vez que o objeto dessa classe for criado eh incrementado um numero
+    significando um novo objeto, do contrario se o incrementador ficasse no objeto o incrementador representaria um determinado
+    numero daquele ojeto e nao de todos da classe criada */
+
+    public function __construct(Titular $titular)  /* dentro de classes as funcoes sao chamadas de metodos. Esse eh o metodo construtor, 
+    deve-se colocar apenas o necessario, e isso compreende a dados, esses nao devem ser colocado diretamente na criacao da classe */
+    {
+        $this->titular = $titular;  // aqui diz: em "Conta" acessando "titular", recebe a variavel titular incrementada
+        $this->saldo = 0;  // o saldo eh iniciado com zero
+
+        self::$numeroDeContas++;  /* "self" eh uma pseudo-variavel, e faz referencia a classe atual. Os "::" foram usado para acessar um 
+        atributo da classe, e nao do objeto */
+    }
+
+    public function __destruct()  /* quando eh excluida a variavel de referencia de um objeto logo esse objeto fica solto na memoria
+    do computador, entao o "Garbage Collector" (Coletor de Lixo) detecta que esse endereco de memoria esta sem uso e o exclui*/
+    {
+        self::$numeroDeContas--;
+    }
+
+    public function saca(float $valorASacar): void  /* "void" significa nao retornar nada, eh importante exclarecer esses casos tambem. 
+    Para fazer o saque ao inves do metodo exigir como parametro a conta criada e o valor a sacar, o "this" facilita, necessitando apenas 
+    o objeto (que eh a conta criada) acessar o metodo (saca) passando como parametro somente o valor a sacar ($valorASacar) */
+    {
+        if ($valorASacar > $this->saldo) {
+            echo "Saldo indisponível";
+            return;
+        }
+
+        $this->saldo -= $valorASacar;  /* "this" eh uma pseudo-variavel, e faz referencia ao objeto atual. A "->" foi usado para acessar
+        um atributo do objeto, e nao da classe */
+    }
+
+    public function deposita(float $valorADepositar): void
+    {
+        if ($valorADepositar < 0) {
+            echo "Valor precisa ser positivo";
+            return;
+        }
+
+        $this->saldo += $valorADepositar;
+    }
+
+    public function transfere(float $valorATransferir, Conta $contaDestino): void
+    {
+        if ($valorATransferir > $this->saldo) {
+            echo "Saldo indisponível";
+            return;
+        }
+
+        $this->saca($valorATransferir);
+        $contaDestino->deposita($valorATransferir);
+    }
+
+    public function recuperaSaldo(): float
+    {
+        return $this->saldo;
+    }
+
+    public function recuperaNomeTitular(): string
+    {
+        return $this->titular->recuperaNome();
+    }
+
+    public function recuperaCpfTitular(): string
+    {
+        return $this->titular->recuperaCpf();
+    }
+
+    public static function recuperaNumeroDeContas(): int
+    {
+        return self::$numeroDeContas;
+    }
+}
